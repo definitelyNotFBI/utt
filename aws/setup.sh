6bfc06e10502963c515ba6c6d18c0d166eaee03b
hermitsage <dth.bht@gmail.com>
2021-08-18 16:59:57 -0400

    Update aws

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,5 +1,9 @@
 for ip in `cat ips.log`; do
-    scp comm_config.txt ubuntu@$ip:~/concord-latest/build/tests/simpleKVBC/scripts/comm_config
-    ssh ubuntu@$ip "cd concord-latest && git pull origin && make -j 16"
+    ssh ubuntu@$ip "cd concord-latest && git pull origin && make" &
     # TODO: Send the local genesis config files to the machines
+done
+wait
+
+for ip in `cat ips.log`; do
+    scp comm_config.txt ubuntu@$ip:~/concord-latest/build/tests/simpleKVBC/scripts/comm_config
 done
